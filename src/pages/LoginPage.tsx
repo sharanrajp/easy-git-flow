c6349abffa3b3705e44e4dbf8aa25bb5ab3cf372
Umass250502 <umass250502@gmail.com>
2025-10-06 18:14:39 +0530

    code removal

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -139,10 +139,6 @@ function LoginPage() {
     }
   }
 
-  const handleForgotPassword = () => {
-    alert("Forgot password functionality is not implemented in this demo.")
-  }
-
   return (
     <div className="min-h-screen flex overflow-hidden">
       {/* Left Side - Image with Diagonal Clip */}
@@ -255,16 +251,6 @@ function LoginPage() {
                 "Sign In"
               )}
             </Button>
-
-            <div className="text-center pt-2">
-              <button
-                type="button"
-                onClick={handleForgotPassword}
-                className="text-sm text-primary hover:text-primary/80 transition-colors font-medium"
-              >
-                Forgot your password?
-              </button>
-            </div>
           </form>
         </div>
       </div>
