7a2be9351410b88542f63f2c512ebc99d351bbba
Umass250502 <umass250502@gmail.com>
2025-10-08 16:11:39 +0530

    changes commit

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -28,7 +28,7 @@ function LoginPage() {
 
     try {
       // Call login endpoint
-      const loginResponse = await fetch("http://127.0.0.1:8000/auth/auth/login", {
+      const loginResponse = await fetch("https://b2ma3tdd2m.us-west-2.awsapprunner.com/auth/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams({
