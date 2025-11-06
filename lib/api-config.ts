56225b741a66074e969c3ad032dabe05728d2591
Patnam Shahija <shahija.p@vuedata.in>
2025-11-06 15:38:37 +0530

    enabled apprunner url

--- lib/api-config.ts
+++ lib/api-config.ts
@@ -2,5 +2,5 @@
  * Centralized API configuration
  * Update this file to change the API base URL for the entire application
  */
-export const API_BASE_URL = "http://127.0.0.1:8000";
-// export const API_BASE_URL = "https://b2ma3tdd2m.us-west-2.awsapprunner.com";
+// export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL = "https://b2ma3tdd2m.us-west-2.awsapprunner.com";
